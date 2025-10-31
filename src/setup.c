5556bd93c0276fcc019ac20643fa97b9bab6f446
Dinamiet <iceman.sm39@gmail.com>
2025-10-31 20:04:33 +0200

    Initialize Notifier List

--- src/setup.c
+++ src/setup.c
@@ -2,10 +2,14 @@
 
 #include <string.h>
 
-void ATTerminal_Init(ATTerminal* at, ATTerminal_Interface read_handler, ATTerminal_Interface write_handler)
+void ATTerminal_Init(ATTerminal* at, ATTerminalResponseNotifier* notifierBuffer, size_t numNotifiers, ATTerminal_Interface read_handler, ATTerminal_Interface write_handler)
 {
 	at->Read_Handler  = read_handler;
 	at->Write_Handler = write_handler;
+
+	BufferedList_Init(&at->NotifierList, notifierBuffer, sizeof(*notifierBuffer), numNotifiers);
+
+	at->Index = 0;
 
 	memset(at->Unprocessed, 0, sizeof(at->Unprocessed));
 }
